469a693a8042582e11d1aaf3a083eb8aee8eb42d
Daniel Arruda <danielp.arruda@gmail.com>
2020-06-15 23:46:34 -0300

    Finished 'Desafio 02'

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newRepository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newRepository);
+
+  return response.status(201).json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0)
+    return response.status(400).json({ error: 'Invalid repository ID.' });
+
+  const newRepository = { 
+    ...repositories[repositoryIndex], 
+    title: title ? title : repositories[repositoryIndex].title, 
+    url: url ? url : repositories[repositoryIndex].url,
+    techs: techs ? techs : repositories[repositoryIndex].techs, 
+  };
+
+  repositories[repositoryIndex] = newRepository;
+
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0)
+    return response.status(400).json({ error: 'Invalid repository ID.' });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0)
+    return response.status(400).json({ error: 'Invalid repository ID.' });
+
+  const newRepository = { ...repositories[repositoryIndex], likes: repositories[repositoryIndex].likes + 1 };
+
+  repositories[repositoryIndex] = newRepository;
+
+  return response.status(201).json(newRepository);
 });
 
 module.exports = app;
